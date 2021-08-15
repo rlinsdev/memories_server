fb5157be58719b93b049ca011a04c1e8da11682a
Rodrigo Lins <rlinsdev@gmail.com>
2021-08-15 19:28:04 -0300

    folder structure - Server

---
+++ README.md
@@ -0,0 +1,8 @@
+# Packages
+* npm init -y
+* npm i body-parser cors express mongoose nodemon
+
+
+
+# DataBase
+* https://www.mongodb.com/cloud/atlas
--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import express from 'express';
-import bodyParcer from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/post.js';
 
-const express= require('express');
+const app = express();
+
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://jsMastery:jsMastery123@cluster0.jjvtx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORR || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
+  .catch((error)=> console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
